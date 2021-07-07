f1a9c866aa7b7fc8f27e5a616de0b673de6fc2aa
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-07-07 14:48:34 +0900

    v.1.0.4
    - button 추가 (3)
     클릭 시 changeText()함수 실행을 통한 Text변환 기능.
    
    - script.js 추가

---
+++ DOGE-ChatBot/script.js
@@ -0,0 +1,13 @@
+function changeText1() {
+    let x = document.getElementById('text1');
+    console.log(x);
+    x.innerText = "모두들 코로나로 인해 많이 힘드시겠지만";
+}
+function changeText2() {
+    let x = document.getElementById('text2');
+    x.innerHTML = "각자 저마다의 방식으로"
+}
+function changeText3() {
+    let x = document.querySelector('.output #text3');
+    x.textContent = "코로나를 잘 이겨내봅시다💥"
+}
