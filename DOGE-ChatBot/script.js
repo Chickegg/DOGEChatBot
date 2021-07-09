5cd4ce6691f7866938d181af51b0585acb7515e8
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-07-09 14:11:24 +0900

    v.1.0.7
    console.log(typeof()) 추가

--- DOGE-ChatBot/script.js
+++ DOGE-ChatBot/script.js
@@ -25,3 +25,21 @@ function reduce() {
     }
 }
 
+
+function datatype() {
+    let a = 1;
+    let b = "1";
+    let c = true;
+    let d = [];
+    let e;
+    let f = {"key": 3};
+    let g = null;
+    // console.log(typeof(a));
+    console.log(typeof(b));
+    console.log(typeof(c));
+    console.log(typeof(d));
+    console.log(typeof(e));
+    console.log(typeof(f));
+    console.log(typeof(g));
+}
+
